d4c88d2aba9922abe9e856acc34fdef6a4197914
aldycavalera <cavalera@sawala.co>
2020-10-28 19:38:46 +0700

    get user path

--- tools/generate.js
+++ tools/generate.js
@@ -2,13 +2,17 @@
 
 const { generateTemplateFiles } = require('generate-template-files')
 
+
+const execSync = require('child_process').execSync;
+const output = execSync('npm root -g', { encoding: 'utf-8' });
+
 generateTemplateFiles([
   // Example of generating a single file
   {
     option: 'Nuxt',
     defaultCase: '(pascalCase)',
     entry: {
-      folderPath: './tools/frameworks/nuxt',
+      folderPath: output+'\tools\frameworks\nuxt',
     },
     stringReplacers: [
         { question: 'Nama Project', slot: '__projectName__' },
